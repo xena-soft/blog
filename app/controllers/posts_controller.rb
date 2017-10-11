fb439c667d258ccfa08b055f3b2a44391bdd19ff
Maxim Ageev <maxsim.ageev@gmail.com>
2017-10-11 18:09:58 +0300

    Добавляю создание нового поста

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,21 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.order(created_at: :desc)
+    render json: @posts
   end
 
   def create
+    @post = Post.new post_params
+    if @post.save
+      render json: @post
+    else
+      render json: { errors: @post.errors.full_messages }
+    end
+  end
+
+  private
+
+  def posts_params
+    require(:post).permit(:header, :content, :user_id, :author_ip)
   end
 end
