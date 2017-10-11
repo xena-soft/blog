7ebdca01164d28768ad5187c23a10b14384e6d60
Maxim Ageev <maxsim.ageev@gmail.com>
2017-10-11 17:20:14 +0300

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011140219) do
+ActiveRecord::Schema.define(version: 20171011140806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "header"
+    t.text "content"
+    t.integer "author_ip"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "login"
